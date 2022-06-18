f945b0e3a3d3fcb7dd93f37b880ef25d277358cc
Nobonet <104540446+Nobonet@users.noreply.github.com>
2022-06-18 06:29:33 -0300

    Update NotificationScript.lua

--- CoreScripts/NotificationScript.lua
+++ CoreScripts/NotificationScript.lua
@@ -3,10 +3,12 @@ function waitForProperty(instance, property)
 		instance.Changed:wait()
 	end
 end
-function waitForChild(instance, name)
+local function waitForChild(instance, name)
 	while not instance:FindFirstChild(name) do
-		instance.ChildAdded:wait()
+		print("waiting for ".. name.. " in ".. instance:GetFullName())
+		task.wait(0.1)
 	end
+	return instance[name]
 end
 
 waitForProperty(game.Players,"LocalPlayer")
