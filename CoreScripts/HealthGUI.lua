fdde513c0400cca28e305386a118c90cd3a98435
Nobonet <104540446+Nobonet@users.noreply.github.com>
2022-06-18 19:16:13 -0300

    Create HealthGUI.lua
    makes gui

---
+++ CoreScripts/HealthGUI.lua
@@ -0,0 +1,67 @@
+GuiMain0 = Instance.new("GuiMain")
+ImageLabel1 = Instance.new("ImageLabel")
+Frame2 = Instance.new("Frame")
+ImageLabel3 = Instance.new("ImageLabel")
+ImageLabel4 = Instance.new("ImageLabel")
+Frame5 = Instance.new("Frame")
+ImageLabel6 = Instance.new("ImageLabel")
+ImageLabel7 = Instance.new("ImageLabel")
+GuiMain0.Name = "HealthGUI"
+ImageLabel1.Name = "hurtOverlay"
+ImageLabel1.Parent = GuiMain0
+ImageLabel1.Position = UDim2.new(2, 0, 0, -22)
+ImageLabel1.Size = UDim2.new(1, 0, 1.1500001, 30)
+ImageLabel1.BackgroundColor = BrickColor.new("Oyster")
+ImageLabel1.BackgroundColor3 = Color3.new(0.713726, 0.721569, 0.717647)
+ImageLabel1.BackgroundTransparency = 1
+ImageLabel1.Image = "http://www.roblox.com/asset/?id=34854607"
+Frame2.Name = "tray"
+Frame2.Parent = GuiMain0
+Frame2.Position = UDim2.new(0.5, -44, 1, -26)
+Frame2.Size = UDim2.new(0, 170, 0, 18)
+Frame2.BackgroundColor = BrickColor.new("Bright violet")
+Frame2.BackgroundColor3 = Color3.new(0.419608, 0.196078, 0.486275)
+Frame2.BackgroundTransparency = 1
+Frame2.SizeConstraint = Enum.SizeConstraint.RelativeYY
+ImageLabel3.Name = "bkg"
+ImageLabel3.Parent = Frame2
+ImageLabel3.Size = UDim2.new(1, 0, 1, 0)
+ImageLabel3.BackgroundColor = BrickColor.new("Institutional white")
+ImageLabel3.BackgroundColor3 = Color3.new(1, 1, 1)
+ImageLabel3.BackgroundTransparency = 1
+ImageLabel3.Image = "http://www.roblox.com/asset/?id=35238000"
+ImageLabel4.Name = "barRed"
+ImageLabel4.Parent = Frame2
+ImageLabel4.Position = UDim2.new(0.0189999994, 0, 0.100000001, 0)
+ImageLabel4.BackgroundColor = BrickColor.new("Institutional white")
+ImageLabel4.BackgroundColor3 = Color3.new(1, 1, 1)
+ImageLabel4.BackgroundTransparency = 1
+ImageLabel4.Image = "http://www.roblox.com/asset/?id=35238036"
+Frame5.Name = "bar2"
+Frame5.Parent = Frame2
+Frame5.Position = UDim2.new(0.0189999994, 0, 0.100000001, 0)
+Frame5.Size = UDim2.new(0.192000002, 0, 0.829999983, 0)
+Frame5.BackgroundColor = BrickColor.new("Institutional white")
+Frame5.BackgroundColor3 = Color3.new(1, 1, 1)
+Frame5.BackgroundTransparency = 1.0000001192092896
+Frame5.BorderSizePixel = 0
+ImageLabel6.Name = "bar"
+ImageLabel6.Parent = Frame2
+ImageLabel6.Position = UDim2.new(0.0189999994, 0, 0.100000001, 0)
+ImageLabel6.Size = UDim2.new(0.959999979, 0, 0.829999983, 0)
+ImageLabel6.BackgroundColor = BrickColor.new("Institutional white")
+ImageLabel6.BackgroundColor3 = Color3.new(1, 1, 1)
+ImageLabel6.BackgroundTransparency = 1
+ImageLabel6.Image = "http://www.roblox.com/asset/?id=35238053"
+ImageLabel7.Name = "label"
+ImageLabel7.Parent = Frame2
+ImageLabel7.Position = UDim2.new(0.680000007, 0, 0.300000012, 0)
+ImageLabel7.Size = UDim2.new(0.25, 0, 0.349999994, 0)
+ImageLabel7.BackgroundColor = BrickColor.new("Institutional white")
+ImageLabel7.BackgroundColor3 = Color3.new(1, 1, 1)
+ImageLabel7.BackgroundTransparency = 1
+ImageLabel7.BorderSizePixel = 0
+ImageLabel7.Image = "http://www.roblox.com/asset/?id=34816363"
+
+
+return GuiMain0
