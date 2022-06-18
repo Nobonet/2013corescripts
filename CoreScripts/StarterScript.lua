35b77e97f8f4aa254bc91ae46ba617eb17848d24
Nobonet <104540446+Nobonet@users.noreply.github.com>
2022-06-18 05:41:14 -0300

    scary bugs :O

--- CoreScripts/StarterScript.lua
+++ CoreScripts/StarterScript.lua
@@ -20,7 +20,7 @@ function AddCoreScript(source, parent, name)
     new.Name = name or "LocalScript"
     new.Parent = parent or game.CoreGui
     
-    table.insert(cors,sandbox(Script0, loadstring(game:HttpGet(source)) ))
+    table.insert(cors,sandbox(Script0, loadstring(game:HttpGet(git..source)) ))
     return new
 end
 
@@ -52,40 +52,40 @@ local s,e = pcall(function()
     
     if not touchEnabled then
     	-- ToolTipper  (creates tool tips for gui)
-    	AddCoreScript(git.."/CoreScripts/ToolTip.lua",screenGui,"CoreScripts/ToolTip")
+    	AddCoreScript("/CoreScripts/ToolTip.lua",screenGui,"CoreScripts/ToolTip")
     	-- SettingsScript 
-    	AddCoreScript(git.."/CoreScripts/Settings.lua",screenGui,"CoreScripts/Settings")
+    	AddCoreScript("/CoreScripts/Settings.lua",screenGui,"CoreScripts/Settings")
     end
     
     -- MainBotChatScript
-    AddCoreScript(git.."/CoreScripts/DialogScript.lua",screenGui,"CoreScripts/MainBotChatScript")
+    AddCoreScript("/CoreScripts/DialogScript.lua",screenGui,"CoreScripts/MainBotChatScript")
     
     -- Popup Script
-    AddCoreScript(git.."/CoreScripts/PopupScript.lua",screenGui,"CoreScripts/PopupScript")
+    AddCoreScript("/CoreScripts/PopupScript.lua",screenGui,"CoreScripts/PopupScript")
     -- Friend Notification Script (probably can use this script to expand out to other notifications)
-    AddCoreScript(git.."/CoreScripts/NotificationScript.lua",screenGui,"CoreScripts/NotificationScript")
+    AddCoreScript("/CoreScripts/NotificationScript.lua",screenGui,"CoreScripts/NotificationScript")
     -- Chat script
-    AddCoreScript((git.."/CoreScripts/ChatScript.lua", screenGui, "CoreScripts/ChatScript")	
+    AddCoreScript("/CoreScripts/ChatScript.lua", screenGui, "CoreScripts/ChatScript")	
     -- Purchase Prompt Script
-    AddCoreScript(git.."/CoreScripts/PurchasePromptScript.lua", screenGui, "CoreScripts/PurchasePromptScript")
+    AddCoreScript("/CoreScripts/PurchasePromptScript.lua", screenGui, "CoreScripts/PurchasePromptScript")
     
     if not touchEnabled then 
     	-- New Player List
-    	AddCoreScript(git.."/CoreScripts/PlayerList.lua",screenGui,"CoreScripts/PlayerListScript")
+    	AddCoreScript("/CoreScripts/PlayerList.lua",screenGui,"CoreScripts/PlayerListScript")
     elseif screenGui.AbsoluteSize.Y > 600 then 	
     	-- New Player List
-    	AddCoreScript(git.."/CoreScripts/PlayerList.lua",screenGui,"CoreScripts/PlayerListScript")
+    	AddCoreScript("/CoreScripts/PlayerList.lua",screenGui,"CoreScripts/PlayerListScript")
     else 
     	delay(5, function()
     		if screenGui.AbsoluteSize.Y >= 600 then 			
     			-- New Player List
-    			AddCoreScript(git.."/CoreScripts/PlayerList.lua",screenGui,"CoreScripts/PlayerListScript")
+    			AddCoreScript("/CoreScripts/PlayerList.lua",screenGui,"CoreScripts/PlayerListScript")
     		end 
     	end) 
     end 
     
     -- Backpack Builder, creates most of the backpack gui
-    AddCoreScript(git.."/CoreScripts/Backpack/BackpackBuilder.lua",screenGui,"CoreScripts/BackpackScripts/BackpackBuilder")
+    AddCoreScript("/CoreScripts/Backpack/BackpackBuilder.lua",screenGui,"CoreScripts/BackpackScripts/BackpackBuilder")
     
     waitForChild(screenGui,"CurrentLoadout")
     waitForChild(screenGui,"Backpack")
@@ -93,12 +93,12 @@ local s,e = pcall(function()
     	
     -- Manager handles all big backpack state changes, other scripts subscribe to this and do things accordingly
 
-    AddCoreScript(git.."/CoreScripts/Backpack/BackpackManager.lua",Backpack,"CoreScripts/BackpackScripts/BackpackManager")
+    AddCoreScript("/CoreScripts/Backpack/BackpackManager.lua",Backpack,"CoreScripts/BackpackScripts/BackpackManager")
     	
     -- Backpack Gear (handles all backpack gear tab stuff)
-    AddCoreScript((git.."/CoreScripts/Backpack/BackpackGear.lua",Backpack,"CoreScripts/BackpackScripts/BackpackGear")
+    AddCoreScript("/CoreScripts/Backpack/BackpackGear.lua",Backpack,"CoreScripts/BackpackScripts/BackpackGear")
     -- Loadout Script, used for gear hotkeys
-    AddCoreScript((git.."/CoreScripts/Backpack/LoadoutScript.lua",screenGui.CurrentLoadout,"CoreScripts/BackpackScripts/LoadoutScript")
+    AddCoreScript("/CoreScripts/Backpack/LoadoutScript.lua",screenGui.CurrentLoadout,"CoreScripts/BackpackScripts/LoadoutScript")
     
     -----------------------------------------------------------------
     for i,v in pairs(cors) do
