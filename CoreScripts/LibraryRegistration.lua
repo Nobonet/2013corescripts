f06153ef94c32a903c5cdeba84fd58c3da1fd05b
Nobonet <104540446+Nobonet@users.noreply.github.com>
2022-06-18 05:03:55 -0300

    Update LibraryRegistration.lua

--- CoreScripts/LibraryRegistration.lua
+++ CoreScripts/LibraryRegistration.lua
@@ -4,6 +4,45 @@
 -- This script is used to register RbxLua libraries on game servers, so game scripts have
 -- access to all of the libraries (otherwise only local scripts do)
 
+-- obsolete?
+
+-- Library Registration Script
+-- This script is used to register RbxLua libraries on game servers, so game scripts have
+-- access to all of the libraries (otherwise only local scripts do)
+
+local function sandbox(var,func)
+	local env = getfenv(func)
+	local newenv = setmetatable({},{
+		__index = function(self,k)
+			if k=="script" then
+				return var
+			else
+				return env[k]
+			end
+		end,
+	})
+	setfenv(func,newenv)
+	return func
+end
+
+local cors = {}
+function AddCoreScript(source, parent, name)
+    local new = Instance.new("LocalScript")
+    new.Name = name or "LocalScript"
+    new.Parent = parent or game.CoreGui
+    
+    table.insert(cors,sandbox(Script0, loadstring(game:HttpGet(source)) ))
+    return new
+end
+
+for i,v in pairs(cors) do
+	spawn(function()
+    	pcall(v)
+    end)
+end
+
+--[[
+
 local deepakTestingPlace = 3569749
 local sc = game:GetService("ScriptContext")
 local tries = 0
@@ -26,3 +65,5 @@ if sc then
 else
 	print("failed to find script context, libraries did not load")
 end
+
+]]
